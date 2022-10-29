fbda1026f3694e4b7771ac7e068513508c73dbe7
Michael Gartner <mclicks@gmail.com>
2022-10-28 19:43:24 -0700

    just commenting everything out to pass JSON string from /server to /client

--- client/src/pages/Authorize.tsx
+++ client/src/pages/Authorize.tsx
@@ -4,36 +4,30 @@ import axios from "axios"
 
 const Authorize = () => {
   const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
-  const [userMetadata, setUserMetadata] = useState(null);
+  const [data, setUserMetadata] = useState(null);
 
   useEffect(() => {
     const getUserMetadata = async () => {
       // const domain = "dev-tdz1zjoqa3dxpzkp.us.auth0.com";
-      let data = {}
       try {
-        const accessToken = await getAccessTokenSilently({
-        //   audience: `https://${domain}/api/v2/`,
-          audience: "http://localhost:8080/",
-          scope: "read:current_user",
-        });
+        // const accessToken = await getAccessTokenSilently({
+          // audience: `https://${domain}/api/v2/`,
+          // audience: "http://localhost:8080",
+          // scope: "read:current_user",
+        // });
   
         // const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
   
-        const metadataResponse = await axios.get("http://localhost:8080/authorize", {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
+        const metadataResponse = await axios.get("http://localhost:8080/authorize");
   
-        // const { user_metadata } = await metadataResponse.json();
-        data = metadataResponse.data;
+        const data = metadataResponse.data;
+        console.log(metadataResponse);
         console.log(data);
-        
+        setUserMetadata(data);
 
-        
-        // setUserMetadata(user_metadata);
       } catch (e) {
         console.log(e.message);
+        console.log(error.response.data)
       }
     };
   
@@ -48,8 +42,8 @@ const Authorize = () => {
         <h2>{user.name}</h2>
         <p>{user.email}</p>
         <h3>User Metadata</h3>
-        {userMetadata ? (
-          <pre>{JSON.stringify(userMetadata, null, 2)}</pre>
+        {data ? (
+          <pre>{JSON.stringify(data, null, 2)}</pre>
         ) : (
           "No user metadata defined"
         )}
