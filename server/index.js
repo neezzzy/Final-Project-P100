e423fc35958c5d1a4133f359431895f5a9d53ac6
Michael Gartner <mclicks@gmail.com>
2022-10-28 19:45:05 -0700

    CORS

--- server/index.js
+++ server/index.js
@@ -9,27 +9,28 @@
 
 var express = require('express');
 var app = express();
-const {expressjwt:jwt} = require('express-jwt');
-const jwks = require('jwks-rsa');
+// const {expressjwt:jwt} = require('express-jwt');
+// const jwks = require('jwks-rsa');
 var port = process.env.PORT || 8080;
+var cors = require('cors');
+app.use(cors());
+// var jwtCheck = jwt({
+//       secret: jwks.expressJwtSecret({
+//           cache: true,
+//           rateLimit: true,
+//           jwksRequestsPerMinute: 5,
+//           jwksUri: 'https://dev-8c172k4g8h4ugh58.us.auth0.com/.well-known/jwks.json'
+//     }),
+//     audience: 'http://localhost:8080',
+//     issuer: 'https://dev-8c172k4g8h4ugh58.us.auth0.com/',
+//     algorithms: ['RS256']
+// });
 
-var jwtCheck = jwt({
-      secret: jwks.expressJwtSecret({
-          cache: true,
-          rateLimit: true,
-          jwksRequestsPerMinute: 5,
-          jwksUri: 'https://dev-8c172k4g8h4ugh58.us.auth0.com/.well-known/jwks.json'
-    }),
-    audience: 'http://localhost:8080',
-    issuer: 'https://dev-8c172k4g8h4ugh58.us.auth0.com/',
-    algorithms: ['RS256']
-});
-
-app.use(jwtCheck);
+// app.use(jwtCheck);
 
 app.get('/authorize', function (req, res) {
-    res.send('Secured Resource');
-    console.log('Secure Resource');
+    // res.send('Secured Resource');
+    // console.log('Secure Resource');
     res.json({
         type: "private"
     })
