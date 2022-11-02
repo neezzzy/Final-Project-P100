a93d82baa722fc859ff90ca7e67807c96bd6b3ba
Michael Gartner <mclicks@gmail.com>
2022-11-02 13:37:20 -0700

    express-generator https://expressjs.com/en/starter/generator.html

--- client/src/pages/Home.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-function Home({}: Props) {
-  return <div>Home</div>;
-}
-
-export default Home;
--- server/messages/messages.permissions.ts
+++
@@ -1,3 +0,0 @@
-export enum AdminMessagesPermissions {
-  Read = "read:admin-messages",
-}
