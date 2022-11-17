03a65a01e0e456cee30ec5d8299aedd2781a9e89
Ruslan Nizamov <nizamov.ruslan@gmail.com>
2022-11-16 21:14:44 -0800

    add third party api key

--- client/controllers/userController.js
+++ client/controllers/userController.js
@@ -1,13 +1,15 @@
 const User = require("../models/userModel");
 const axios = require("axios");
+require("dotenv").config();
 
 module.exports = {
   fetchUserProfile: async function (req, res) {
     let userAuth0 = req.oidc.user;
     try {
       const user = await User.findOne({ email: userAuth0.email });
+      // get your bloody API key here https://api.orb-intelligence.com/docs/
       const response = await axios.get(
-        `https://api.orb-intelligence.com/3/match/?api_key=c66c5dad-395c-4ec6-afdf-7b78eb94166a&name=${user.name}&country=${user.country}`
+        `https://api.orb-intelligence.com/3/match/?api_key=${process.env.ORB_API_KEY}&name=${user.name}&country=${user.country}`
       );
 
       res.render("profile", {
